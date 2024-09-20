59c73bcffacd34605d072f92bbb2329a56428733
1125likitha <likitha2511@gmail.com>
2024-09-20 15:47:52 +0530

    added new and delete

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+    Passenger passengers[3];
+
+    passengers[0].setName("Bob");
+    passengers[1].setName("Alice");
+    passengers[2].setName("Charlie");
+
+    for (int i = 0; i < 3; i++) {
+        passengers[i].displayInfo();
+    }
