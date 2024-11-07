f74c2e447f435075398f034472b3a97e9d68c9c3
1125likitha <likitha2511@gmail.com>
2024-11-07 13:04:04 +0530

    done

--- project.cpp
+++ project.cpp
@@ -2,7 +2,13 @@
 #include <string>
 using namespace std;
 
-class Train {
+// Abstract class with a pure virtual function
+class TrainInfo {
+public:
+    virtual void displayScheduleDetails() = 0;  // Pure virtual function
+};
+
+class Train : public TrainInfo {
 private:
     int scheduleHour;
     int scheduleMinute;
@@ -11,16 +17,14 @@ protected:
     string trainName;
 
 public:
-    // Original setSchedule function with hour and minute parameters
     void setSchedule(int hour, int minute) {
         scheduleHour = hour;
         scheduleMinute = minute;
     }
 
-    // Overloaded setSchedule function with only hour parameter
     void setSchedule(int hour) {
         scheduleHour = hour;
-        scheduleMinute = 0;  // Default minutes to 0 if only hour is provided
+        scheduleMinute = 0;
     }
 
     void getSchedule() {
@@ -31,70 +35,37 @@ public:
     void setTrainName(string name) {
         trainName = name;
     }
+
+    // Override the pure virtual function from TrainInfo
+    void displayScheduleDetails() override {
+        cout << "Train Name: " << trainName << ", Scheduled Time: "
+             << scheduleHour << ":" << (scheduleMinute < 10 ? "0" : "") << scheduleMinute << endl;
+    }
+
+    virtual void showFeatures() {
+        cout << "Basic train features for " << trainName << "." << endl;
+    }
 };
 
 class LuxuryTrain : public Train {
 public:
-    void displayLuxuryFeatures() {
+    void showFeatures() override {
         cout << trainName << " is a luxury train with premium features." << endl;
-    }
-};
-
-class Passenger {
-private:
-    string name;
-
-protected:
-    int passengerID;
-
-public:
-    void setName(string passengerName) {
-        name = passengerName;
-    }
-
-    void getName() {
-        cout << "Passenger Name: " << name << " (ID: " << passengerID << ")" << endl;
-    }
-
-    void setPassengerID(int id) {
-        passengerID = id;
-    }
-};
-
-class VIPPassenger : public Passenger {
-public:
-    void displayVIPStatus() {
-        cout << "VIP Passenger ID: " << passengerID << endl;
     }
 };
 
 int main() {
     Train train1;
     train1.setTrainName("Express");
-    train1.setSchedule(14, 30);  // Calls setSchedule with hour and minute
-    train1.getSchedule();
-
-    Train train2;
-    train2.setTrainName("FastTrack");
-    train2.setSchedule(12);  // Calls overloaded setSchedule with only hour
-    train2.getSchedule();
+    train1.setSchedule(14, 30);
+    train1.displayScheduleDetails();
+    train1.showFeatures();
 
     LuxuryTrain luxuryTrain1;
     luxuryTrain1.setTrainName("Titan");
     luxuryTrain1.setSchedule(16, 45);
-    luxuryTrain1.getSchedule();
-    luxuryTrain1.displayLuxuryFeatures();
-
-    Passenger passenger1;
-    passenger1.setName("John Doe");
-    passenger1.setPassengerID(1234);
-    passenger1.getName();
-
-    VIPPassenger vipPassenger;
-    vipPassenger.setName("Jane Doe");
-    vipPassenger.setPassengerID(5678);
-    vipPassenger.getName();
-    vipPassenger.displayVIPStatus();
+    luxuryTrain1.displayScheduleDetails();
+    luxuryTrain1.showFeatures();
 
     return 0;
 }
